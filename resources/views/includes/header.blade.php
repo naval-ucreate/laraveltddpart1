51730076ce31149c21b16e6bde0b157635050638
ucreate-74 <naval@ucreate.co.in>
2019-02-04 10:54:10 +0530

    change in header.balde.php

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -9,7 +9,6 @@
       <li><a href="#">Page 2</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-
       @guest
       <li><a href="<?php echo route('display-register-page'); ?>"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
       <li><a href="<?php echo route('display-login-page'); ?>"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
@@ -17,8 +16,6 @@
       <li><a href="<?php echo route('display-register-page'); ?>"><span class="glyphicon glyphicon-user">{{ Auth::user()->name }}</span> </a></li>
       <li><a href="<?php echo route('logout'); ?>"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
       @endguest;
-
-
     </ul>
   </div>
 </nav>
