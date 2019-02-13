220881e4696126a464b2e836bbda9206728a407c
ucreate-71 <naval@ucreate.co.in>
2019-02-13 11:57:42 +0530

    fix user model issue

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -4,12 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Laravel\Passport\HasApiTokens;
-use Illuminate\Notifications\Notifiable;
 
 class Users extends Authenticatable
 {
-  
-    use HasApiTokens, Notifiable;
     protected $table = 'users';
 }
